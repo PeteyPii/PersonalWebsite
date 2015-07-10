e9163d7452d6ce43442ebb020cc27eddb365cd95
Patrick Wrobel <craptik@gmail.com>
2015-07-10 02:34:02 -0400

    Did some simple stuff with the canvas

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -4,13 +4,6 @@ controllers.controller('HomeController', ['$scope',
   function ($scope) {
     $('.navbar-nav>li').removeClass('active');
     $('.navbar-nav>li#nav-home').addClass('active');
-
-    $('#begin-game-btn').click(function() {
-      var $body = $('body');
-      document.body.scrollTop = 0; // pull the page back up to the top
-      $body.addClass('game-mode');
-      $body.html('<canvas id="game-canvas"></canvas>');
-    });
   }
 ]);
 
@@ -38,5 +31,43 @@ controllers.controller('AboutController', ['$scope',
 controllers.controller('MissingPageController', ['$scope',
   function ($scope) {
     $('.navbar-nav>li').removeClass('active');
+  }
+]);
+
+controllers.controller('GameController', ['$scope',
+  function ($scope) {
+    $('#begin-game-btn').click(function() {
+      $('.main-site').hide();
+      document.body.scrollTop = 0; // pull the page back up to the top
+      $('body').addClass('game-mode');
+      $('#game-container').show();
+
+      var canvas = document.getElementById('game-canvas')
+      var isSupported = !!canvas.getContext;
+      var ctx = canvas.getContext('2d');
+
+      function draw() {
+        if (isSupported) {
+          var width  = ctx.canvas.width  = window.innerWidth;
+          var height = ctx.canvas.height = window.innerHeight;
+
+          ctx.fillStyle = '#000000';
+          ctx.fillRect(0, 0, width, height);
+
+          var path = new Path2D();
+
+          path.moveTo(width / 2 + 25, height / 2);
+          path.lineTo(width / 2, height / 2 + 25);
+          path.lineTo(width / 2 - 25, height / 2);
+          path.lineTo(width / 2, height / 2 - 25);
+          path.lineTo(width / 2 + 25, height / 2);
+
+          ctx.fillStyle = 'white';
+          ctx.fill(path);
+        }
+      }
+
+      draw();
+    });
   }
 ]);
