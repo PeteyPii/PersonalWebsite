9b7428b94aa7d2c9174b4d8ae8417a17bf2f77f9
Patrick Wrobel <pwrobel@uwaterloo.ca>
2015-07-10 01:16:57 -0400

    Began working on a tiny game

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -4,6 +4,13 @@ controllers.controller('HomeController', ['$scope',
   function ($scope) {
     $('.navbar-nav>li').removeClass('active');
     $('.navbar-nav>li#nav-home').addClass('active');
+
+    $('#begin-game-btn').click(function() {
+      var $body = $('body');
+      document.body.scrollTop = 0; // pull the page back up to the top
+      $body.addClass('game-mode');
+      $body.html('<canvas id="game-canvas"></canvas>');
+    });
   }
 ]);
 
