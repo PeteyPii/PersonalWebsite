2668966a48f6cc91fdba402a688ca6ba45c55891
Patrick Wrobel <pwrobel@uwaterloo.ca>
2015-11-17 23:37:04 -0800

    Added more stats to LoL API

--- api.js
+++ api.js
@@ -34,23 +34,105 @@ function updateCache(value, callback) {
         callback(null);
       });
       break;
+
     case 'lol':
+      var lol = {};
       request.get('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + settings.summoner_name + '?api_key=' + settings.lol_api_key, function(err, resp, body) {
         if (err) {
           callback(err)
           return;
         }
 
-        var data = JSON.parse(body);
-        var summonerId = JSON.parse(body)[settings.summoner_name.toLowerCase()].id;
+        var summonerData = JSON.parse(body);
+        var summonerId = summonerData[settings.summoner_name.toLowerCase()].id;
+        lol.summonerId = summonerId;
 
-        cache.lol = {
-          summonerId: data[settings.summoner_name.toLowerCase()].id,
-        };
-        lastUpdates[value] = (new Date()).getTime();
-        callback(null);
+        var hadErr = false;
+        var successes = 2;
+
+        request.get('https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/' + summonerId + '/ranked?api_key=' + settings.lol_api_key, function(err, resp, body) {
+          if (hadErr) {
+            return;
+          }
+
+          if (err) {
+            hadErr = true;
+            callback(err);
+            return;
+          }
+
+          var rankedData = JSON.parse(body);
+          var allStats;
+          for (var i = 0; i < rankedData.champions.length; i++) {
+            if (rankedData.champions[i].id == 0) {
+              allStats = rankedData.champions[i].stats;
+            }
+          }
+
+          lol.rankedGamesPlayed = allStats.totalSessionsPlayed;
+          lol.rankedAssists = allStats.totalAssists;
+          lol.rankedKills = allStats.totalChampionKills;
+          lol.rankedCreepScore = allStats.totalMinionKills + allStats.totalNeutralMinionsKilled;
+
+          successes -= 1;
+          if (successes === 0) {
+            cache.lol = lol;
+            lastUpdates[value] = (new Date()).getTime();
+            callback(null);
+          }
+        });
+
+        request.get('https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/' + summonerId + '/summary?api_key=' + settings.lol_api_key, function(err, resp, body) {
+          if (hadErr) {
+            return;
+          }
+
+          if (err) {
+            hadErr = true;
+            callback(err);
+            return;
+          }
+
+          var summaryData = JSON.parse(body);
+          for (var i = 0; i < summaryData.playerStatSummaries.length; i++) {
+            var statSummary = summaryData.playerStatSummaries[i];
+            switch (statSummary.playerStatSummaryType) {
+              case 'Unranked':
+                lol.normalWins = statSummary.wins;
+                lol.normalCreepScore = statSummary.aggregatedStats.totalMinionKills + statSummary.aggregatedStats.totalNeutralMinionsKilled;
+                lol.normalAssists = statSummary.aggregatedStats.totalAssists;
+                lol.normalKills = statSummary.aggregatedStats.totalChampionKills;
+                lol.normalTurretKills = statSummary.aggregatedStats.totalTurretsKilled;
+                break;
+
+              case 'RankedSolo5x5':
+                lol.soloQueueWins = statSummary.wins;
+                lol.soloQueueLosses = statSummary.losses;
+                lol.soloQueueCreepScore = statSummary.aggregatedStats.totalMinionKills + statSummary.aggregatedStats.totalNeutralMinionsKilled;
+                lol.soloQueueAssists = statSummary.aggregatedStats.totalAssists;
+                lol.soloQueueKills = statSummary.aggregatedStats.totalChampionKills;
+                lol.soloQueueTurretKills = statSummary.aggregatedStats.totalTurretsKilled;
+                break;
+
+              case 'AramUnranked5x5':
+                lol.aramWins = statSummary.wins;
+                lol.aramAssists = statSummary.aggregatedStats.totalAssists;
+                lol.aramKills = statSummary.aggregatedStats.totalChampionKills;
+                lol.aramTurretKills = statSummary.aggregatedStats.totalTurretsKilled;
+                break;
+            }
+          }
+
+          successes -= 1;
+          if (successes === 0) {
+            cache.lol = lol;
+            lastUpdates[value] = (new Date()).getTime();
+            callback(null);
+          }
+        });
       });
       break;
+
     default:
       throw Error('Value not handled in cache \'' + value + '\'');
       break;
