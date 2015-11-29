391342ee6a91879bcb3b029bf25e20df2fc7b9c1
Patrick Wrobel <pwrobel@uwaterloo.ca>
2015-11-29 02:02:24 -0800

    Worked on LoL stats on about me page

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -26,10 +26,56 @@ controllers.controller('ResumeController', ['$scope',
   }
 ]);
 
-controllers.controller('AboutController', ['$scope',
-  function ($scope) {
+controllers.controller('AboutController', ['$scope', '$http',
+  function ($scope, $http) {
     $('.navbar-nav>li').removeClass('active');
     $('.navbar-nav>li#nav-about').addClass('active');
+
+    aramTakedownsBreakpoints = [100, 500, 2500, 5000, 10000];
+    aramWinsBreakpoints = [5, 25, 100, 300, 600];
+    aramTowersBreakpoints = [5, 25, 100, 200, 500];
+
+    function getBreakpoint(value, breakpoints) {
+      for (var i = 0; i < breakpoints.length; i++) {
+        if (value <= breakpoints[i]) {
+          return i;
+        }
+      }
+
+      return breakpoints.length;
+    }
+
+    breakpointToMedal = [
+      'none',
+      'bronze',
+      'silver',
+      'gold',
+      'platinum',
+      'diamond',
+    ];
+
+    function medalImage(value, breakpoints) {
+      return '/imgs/lolmedals/' + breakpointToMedal[getBreakpoint(value, breakpoints)] + '.png';
+    }
+
+    function aramTakedownsMedal() {
+      return medalImage($scope.lol.aramKills + $scope.lol.aramAssists, aramTakedownsBreakpoints);
+    }
+
+    function aramWinsMedal() {
+      return medalImage($scope.lol.aramWins, aramWinsBreakpoints);
+    }
+
+    function aramTowersMedal() {
+      return medalImage($scope.lol.aramTurretKills, aramTowersBreakpoints);
+    }
+
+    $http.get('/api/LoL').success(function(data) {
+      $scope.lol = data;
+      $scope.aramTakedownsMedal = aramTakedownsMedal;
+      $scope.aramWinsMedal = aramWinsMedal;
+      $scope.aramTowersMedal = aramTowersMedal;
+    });
   }
 ])
 
